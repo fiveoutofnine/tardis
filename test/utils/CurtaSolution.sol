55316f7d4e984715765bc85016caac0b0d49f951
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-08-18 12:30:19 -0400

    feat: add base set up solution contract

--- test/utils/CurtaSolution.sol
+++ test/utils/CurtaSolution.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.26;
 
 import {Test} from "forge-std/Test.sol";
 
@@ -10,27 +10,23 @@ import {IPuzzle} from "curta/interfaces/IPuzzle.sol";
 import {MockPuzzle} from "curta/utils/mock/MockPuzzle.sol";
 import {LibRLP} from "curta/utils/LibRLP.sol";
 
-/// @notice A base test contract for Curta. In `setUp`, it deploys an instance
-/// of `AuthorshipToken` and `Curta`. Additionally, it funds 2 addresses
-/// `0xBEEF` and `0xC0FFEE` with 1000 ether each for testing. It also contains a
-/// few commonly used helper functions.
+/// @notice A solution setup contract for Curta. In `setUp`, it deploys an
+/// instance of Curta contracts, labels addresses, and has a helper function to
+/// deploy and add mock puzzles to Curta to skip to a given puzzle ID.
 abstract contract CurtaSolution is Test {
     // -------------------------------------------------------------------------
-    // Constants
+    // Immutable storage
     // -------------------------------------------------------------------------
 
-    /// @notice Owner of the Authorship Token contract.
-    address constant AUTHORSHIP_TOKEN_OWNER = address(0xC0FFEEBABE);
+    /// @notice ID of the environment's chain.
+    uint256 internal immutable chainId;
 
-    /// @notice Owner of the Curta contract.
-    address constant CURTA_OWNER = address(0xDECAFC0FFEE);
+    /// @notice Address of the mock author used to add puzzles to skip to a
+    /// given puzzle ID.
+    address internal immutable mockAuthor;
 
-    /// @notice The number of seconds until an additional token is made
-    /// available for minting by the author.
-    uint256 constant ISSUE_LENGTH = 3 days;
-
-    /// @notice The list of authors in the initial batch.
-    address[] internal AUTHORS = new address[](0);
+    /// @notice Address of the owner of the contracts.
+    address internal immutable owner;
 
     // -------------------------------------------------------------------------
     // Contracts
@@ -39,87 +35,96 @@ abstract contract CurtaSolution is Test {
     /// @notice The Authorship Token contract.
     AuthorshipToken internal authorshipToken;
 
-    /// @notice The Flag metadata and art renderer contract.
-    FlagRenderer internal flagRenderer;
-
     /// @notice The Curta contract.
     Curta internal curta;
 
+    /// @notice The Flag metadata and art renderer contract.
+    FlagRenderer internal flagRenderer;
+
     /// @notice A mock puzzle contract.
-    /// @dev This instance of `MockPuzzle` is just used for its functions (i.e.
-    /// not directly accessed in tests).
     MockPuzzle internal mockPuzzle;
 
     // -------------------------------------------------------------------------
     // Setup
     // -------------------------------------------------------------------------
 
+    /// @notice Sets addresses, labels addresses, and sets the environment
+    /// variables.
+    constructor(uint256 _chainId) {
+        // Set addresses.
+        mockAuthor = makeAddr("mockAuthor");
+        owner = makeAddr("owner");
+        // Label addresses.
+        vm.label(mockAuthor, "Mock author");
+        vm.label(owner, "Owner");
+        // Set environment variables.
+        chainId = _chainId;
+    }
+
+    /// @notice Deploys an instance of the Curta contracts and labels them.
     function setUp() public {
         // Transaction #1.
         flagRenderer = new FlagRenderer();
+        vm.label(address(flagRenderer), "`FlagRenderer`");
 
         // Curta will be deployed on transaction #3.
         address curtaAddress = LibRLP.computeAddress(address(this), 3);
 
         // Transaction #2.
-        authorshipToken = new AuthorshipToken(
-            curtaAddress,
-            ISSUE_LENGTH,
-            AUTHORS
-        );
+        address[] memory authors = new address[](0);
+        authorshipToken = new AuthorshipToken({
+            _curta: curtaAddress,
+            _issueLength: 3 days,
+            _authors: authors
+        });
+        vm.label(address(authorshipToken), "`AuthorshipToken`");
 
         // Transaction #3.
         curta = new Curta(authorshipToken, flagRenderer);
-
-        vm.deal(address(0xBEEF), 1000 ether);
-        vm.deal(address(0xC0FFEE), 1000 ether);
+        vm.label(address(curta), "`Curta`");
 
         // Transaction #4.
         mockPuzzle = new MockPuzzle();
+        vm.label(address(mockPuzzle), "`MockPuzzle`");
 
         // Transfer ownership of the contracts to their respective owners.
-        authorshipToken.transferOwnership(AUTHORSHIP_TOKEN_OWNER);
-        curta.transferOwnership(CURTA_OWNER);
+        authorshipToken.transferOwnership(owner);
+        curta.transferOwnership(owner);
+
+        // Finally, set the environment variables in the VM.
+        vm.chainId(chainId);
     }
 
     // -------------------------------------------------------------------------
     // Helper Functions
     // -------------------------------------------------------------------------
 
-    /// @notice Deploys and adds a puzzle to Curta.
-    /// @param _as The address to deploy the puzzle as.
-    function _deployAndAddPuzzle(address _as) internal {
-        MockPuzzle puzzle = new MockPuzzle();
-        _mintAuthorshipToken(_as);
+    /// @notice Deploys and adds `_id - 1` instances of `mockPuzzle` as puzzles
+    /// to Curta to skip to that ID.
+    /// @param _id The ID of the puzzle to skip to.
+    function _skipToPuzzleId(uint32 _id) internal {
+        // Return early if `_id` is 0.
+        if (_id == 0) return;
 
-        vm.startPrank(_as);
-        curta.addPuzzle(IPuzzle(puzzle), authorshipToken.totalSupply());
-        vm.stopPrank();
-    }
+        // Deploy and add `_id - 1` instances of `mockPuzzle`.
+        unchecked {
+            uint256 count = _id - 1;
+            for (uint256 i; i < count; ++i) {
+                // Mint an Authorship Token to the mock author.
+                vm.prank(address(curta));
+                authorshipToken.curtaMint(mockAuthor);
 
-    /// @notice Mints an Authorship Token to `_to` by acting as Curta.
-    /// @param _to The address to mint the token to.
-    function _mintAuthorshipToken(address _to) internal {
-        vm.prank(address(curta));
-
-        authorshipToken.curtaMint(_to);
-    }
-
-    /// @notice Submits a solution to the puzzle of ID `_puzzleId` as `_as`.
-    /// @dev The puzzle is assumed to be `MockPuzzle`.
-    /// @param _puzzleId The ID of the puzzle to solve.
-    /// @param _as The address to solve the puzzle as.
-    function _solveMockPuzzle(uint32 _puzzleId, address _as) internal {
-        uint256 solution = mockPuzzle.getSolution(_as);
-
-        vm.startPrank(_as);
-        curta.solve(_puzzleId, solution);
-        vm.stopPrank();
+                // Add the puzzle to Curta.
+                vm.prank(mockAuthor);
+                curta.addPuzzle(IPuzzle(mockPuzzle), i);
+            }
+        }
     }
 
     // -------------------------------------------------------------------------
     // Interface
     // -------------------------------------------------------------------------
 
+    /// @notice Test function for players to implement their solution.
     function test_solve() public virtual;
 }
