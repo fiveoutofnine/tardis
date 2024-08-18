3dc8232628276533cb72885bf1173200a407112d
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2024-08-18 12:51:34 -0400

    fix: add some extra auto-generated information for players

--- script/curta/setup.sh
+++ script/curta/setup.sh
@@ -1,9 +1,9 @@
 # Helper function to create solution files for Curta puzzles.
 create_file() {
-    local id="$1"
-    local base_dir="$2"
-    local setup_path="${base_dir}/${id}/Setup.sol"
-    local file_path="${base_dir}/${id}/Solution.t.sol"
+    local network="$1"
+    local id="$2"
+    local setup_path="test/curta/${network}/${id}/Setup.sol"
+    local file_path="test/curta/${network}/${id}/Solution.t.sol"
 
     # Check if the setup file exists and exit if it doesn't.
     if [ ! -f "$setup_path" ]; then
@@ -22,6 +22,7 @@ pragma solidity ^0.8.26;
 
 import {Setup} from "./Setup.sol";
 
+/// @dev View the puzzle's source at \`src/curta/$network/$id\`.
 contract Solution is Setup {
     function test_solve() public virtual override {
         // <<<<<<< SOLUTION START.
@@ -39,6 +40,6 @@ EOF
 
 # Create solution files.
 for id in {1..100}; do
-    create_file "$id" "test/curta/base"
-    create_file "$id" "test/curta/eth"
+    create_file "base" "$id"
+    create_file "eth" "$id"
 done
